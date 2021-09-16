5a8c3e5e312a9b3b9e741c7d67e3d8f74c073934
Alex Eaton <alex@Alexs-MacBook-Air.local>
2021-09-15 20:13:16 -0400

    Updated stash options

--- stash/stash_gen.go
+++ stash/stash_gen.go
@@ -9,120 +9,82 @@ import (
 	"github.com/1800alex/go-git-cmd-wrapper/v2/types"
 )
 
-// Abort Abort/reset an in-progress git notes merge, i.e. a notes merge with conflicts.
-// This simply removes all files related to the notes merge.
-// --abort
-func Abort(g *types.Cmd) {
-	g.AddOptions("--abort")
+// All This option is only valid for push and save commands.
+// All ignored and untracked files are also stashed and then cleaned up with git clean.
+// -a, --all
+func All(g *types.Cmd) {
+	g.AddOptions("--all")
 }
 
-// AllowEmpty Allow an empty note object to be stored. The default behavior is to automatically remove empty notes.
-// --allow-empty
-func AllowEmpty(g *types.Cmd) {
-	g.AddOptions("--allow-empty")
+// IncludeUntracked When used with the push and save commands, all untracked files are also stashed and then cleaned up with git clean.
+// When used with the show command, show the untracked files in the stash entry as part of the diff.
+// -u, --include-untracked
+func IncludeUntracked(g *types.Cmd) {
+	g.AddOptions("--include-untracked")
 }
 
-// Commit Finalize an in-progress git notes merge.
-// Use this option when you have resolved the conflicts that git notes merge stored in .git/NOTES_MERGE_WORKTREE.
-// This amends the partial merge commit created by git notes merge (stored in .git/NOTES_MERGE_PARTIAL) by adding the notes in .git/NOTES_MERGE_WORKTREE.
-// The notes ref stored in the .git/NOTES_MERGE_REF symref is updated to the resulting commit.
-// --commit
-func Commit(g *types.Cmd) {
-	g.AddOptions("--commit")
+// Index This option is only valid for pop and apply commands.
+// Tries to reinstate not only the working tree’s changes, but also the index’s ones. However, this can fail, when you have conflicts (which are stored in the index, where you therefore can no longer apply the changes as they were originally).
+// --index
+func Index(g *types.Cmd) {
+	g.AddOptions("--index")
 }
 
-// DryRun Do not remove anything; just report the object names whose notes would be removed.
-// -n, --dry-run
-func DryRun(g *types.Cmd) {
-	g.AddOptions("--dry-run")
+// KeepIndex This option is only valid for push and save commands.
+// All changes already added to the index are left intact.
+// -k, --keep-index
+func KeepIndex(g *types.Cmd) {
+	g.AddOptions("--keep-index")
 }
 
-// File Take the note message from the given file.
-// Use - to read the note message from the standard input.
-// Lines starting with # and empty lines other than a single line between paragraphs will be stripped out.
-// -F <file>, --file=<file>
-func File(value string) func(*types.Cmd) {
+// NoIncludeUntracked When used with the push and save commands, all untracked files are also stashed and then cleaned up with git clean.
+// When used with the show command, show the untracked files in the stash entry as part of the diff.
+// --no-include-untracked
+func NoIncludeUntracked(g *types.Cmd) {
+	g.AddOptions("--no-include-untracked")
+}
+
+// NoKeepIndex This option is only valid for push and save commands.
+// All changes already added to the index are left intact.
+// --no-keep-index
+func NoKeepIndex(g *types.Cmd) {
+	g.AddOptions("--no-keep-index")
+}
+
+// OnlyUntracked This option is only valid for the show command.
+// Show only the untracked files in the stash entry as part of the diff.
+// --only-untracked
+func OnlyUntracked(g *types.Cmd) {
+	g.AddOptions("--only-untracked")
+}
+
+// Patch This option is only valid for push and save commands.
+// Interactively select hunks from the diff between HEAD and the working tree to be stashed. The stash entry is constructed such that its index state is the same as the index state of your repository, and its worktree contains only the changes you selected interactively. The selected changes are then rolled back from your worktree. See the “Interactive Mode” section of git-add[1] to learn how to operate the --patch mode.
+// The --patch option implies --keep-index. You can use --no-keep-index to override this.
+// -p, --patch
+func Patch(g *types.Cmd) {
+	g.AddOptions("--patch")
+}
+
+// PathspecFileNul This option is only valid for push command.
+// Only meaningful with --pathspec-from-file. Pathspec elements are separated with NUL character and all other characters are taken literally (including newlines and quotes).
+// --pathspec-file-nul
+func PathspecFileNul(g *types.Cmd) {
+	g.AddOptions("--pathspec-file-nul")
+}
+
+// PathspecFromFile This option is only valid for push command.
+// Pathspec is passed in <file> instead of commandline args. If <file> is exactly - then standard input is used. Pathspec elements are separated by LF or CR/LF. Pathspec elements can be quoted as explained for the configuration variable core.quotePath (see git-config[1]). See also --pathspec-file-nul and global --literal-pathspecs.
+// --pathspec-from-file=<file>
+func PathspecFromFile(file string) func(*types.Cmd) {
 	return func(g *types.Cmd) {
-		g.AddOptions(fmt.Sprintf("--file=%s", value))
+		g.AddOptions(fmt.Sprintf("--pathspec-from-file=%s", file))
 	}
 }
 
-// Force When adding notes to an object that already has notes, overwrite the existing notes (instead of aborting).
-// -f, --force
-func Force(g *types.Cmd) {
-	g.AddOptions("--force")
-}
-
-// IgnoreMissing Do not consider it an error to request removing notes from an object that does not have notes attached to it.
-// --ignore-missing
-func IgnoreMissing(g *types.Cmd) {
-	g.AddOptions("--ignore-missing")
-}
-
-// Message Use the given note message (instead of prompting).
-// If multiple -m options are given, their values are concatenated as separate paragraphs.
-// Lines starting with # and empty lines other than a single line between paragraphs will be stripped out.
-// -m <msg>, --message=<msg>
-func Message(msg string) func(*types.Cmd) {
-	return func(g *types.Cmd) {
-		g.AddOptions(fmt.Sprintf("--message=%s", msg))
-	}
-}
-
-// Quiet When merging notes, operate quietly.
-// -q, --quiet
+// Quiet This option is only valid for apply, drop, pop, push, save, store commands.
+// Quiet, suppress feedback messages.
+// -q,
 func Quiet(g *types.Cmd) {
 	g.AddOptions("--quiet")
-}
-
-// ReeditMessage Like -C, but with -c the editor is invoked, so that the user can further edit the note message.
-// -c <object>, --reedit-message=<object>
-func ReeditMessage(object string) func(*types.Cmd) {
-	return func(g *types.Cmd) {
-		g.AddOptions(fmt.Sprintf("--reedit-message=%s", object))
-	}
-}
-
-// Ref Manipulate the notes tree in <ref>. This overrides GIT_NOTES_REF and the "core.notesRef" configuration.
-// The ref specifies the full refname when it begins with refs/notes/;
-// when it begins with notes/, refs/ and otherwise refs/notes/ is prefixed to form a full name of the ref.
-// --ref <ref>
-func Ref(value string) func(*types.Cmd) {
-	return func(g *types.Cmd) {
-		g.AddOptions("--ref")
-		g.AddOptions(value)
-	}
-}
-
-// ReuseMessage Take the given blob object (for example, another note) as the note message.
-// (Use git notes copy <object> instead to copy notes between objects.)
-// -C <object>, --reuse-message=<object>
-func ReuseMessage(object string) func(*types.Cmd) {
-	return func(g *types.Cmd) {
-		g.AddOptions(fmt.Sprintf("--reuse-message=%s", object))
-	}
-}
-
-// Stdin Also read the object names to remove notes from the standard input (there is no reason you cannot combine this with object names from the command line).
-// --stdin
-func Stdin(g *types.Cmd) {
-	g.AddOptions("--stdin")
-}
-
-// Strategy When merging notes, resolve notes conflicts using the given strategy.
-// The following strategies are recognized: "manual" (default), "ours", "theirs", "union" and "cat_sort_uniq".
-// This option overrides the "notes.mergeStrategy" configuration setting.
-// See the "NOTES MERGE STRATEGIES" section below for more information on each notes merge strategy.
-// -s <strategy>, --strategy=<strategy>
-func Strategy(value string) func(*types.Cmd) {
-	return func(g *types.Cmd) {
-		g.AddOptions(fmt.Sprintf("--strategy=%s", value))
-	}
-}
-
-// Verbose When merging notes, be more verbose.
-// When pruning notes, report all object names whose notes are removed.
-// -v, --verbose
-func Verbose(g *types.Cmd) {
-	g.AddOptions("--verbose")
 }
